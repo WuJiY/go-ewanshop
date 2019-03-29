67cd25b732f2d194268812612919c216f9df87fe
haolifeng <haolifengwang@126.com>
2019-03-29 13:00:04 +0800

    clear all

--- routes/admin/logout.go
+++
@@ -1,9 +0,0 @@
-package admin
-
-import "github.com/haolifeng/go-testShop/framework"
-
-func Logout(ctx *framework.HandlerContext) {
-	// ctx.SetSessionVal("isAdmin", false)
-	ctx.SetSessionVal("isAdmin", true)
-	ctx.Redirect("./index.go")
-}
